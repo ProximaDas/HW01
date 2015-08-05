28d179d5d8895bb3a260fba5f96ac4a515266991
ProximaDas <prox.me@gmail.com>
2015-08-04 21:59:31 -0700

    HW01

--- HW01_ex01_04.py
+++ HW01_ex01_04.py
@@ -8,4 +8,13 @@
 # If you run a 10 kilometer race in 43 minutes 30 seconds, what is your 
 # average time per mile? What is your average speed in miles per hour? 
 # (Hint: there are 1.61 kilometers in a mile).
-# Average Speed in MPH:
+# Average Speed in MPH: 8.56714499893
+
+miles = 10/1.61
+print miles
+time = 43.0/60 + 30.0/3600
+print time
+tpm = time/miles
+print 'Time per mile: ' + str(tpm)
+mph = miles/time
+print 'Miles per hour: ' + str(mph)
